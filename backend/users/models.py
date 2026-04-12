e1d76cf8de43808dbad0781c00e4a6fd1f172aaf
Golovanov Sergey <golovanov911@gmail.com>
2026-04-12 15:18:07 +0300

    Правки по комментариям

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='Email')
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, verbose_name='Аватар')
+    avatar = models.ImageField(upload_to='avatars/', 
+                               null=True, blank=True, verbose_name='Аватар'
+                               )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
@@ -15,13 +18,23 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
+
 class Follow(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower', verbose_name='Подписчик')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following', verbose_name='Автор')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name='follower',
+                             verbose_name='Подписчик'
+                             )
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='following',
+                               verbose_name='Автор'
+                               )
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['user', 'author'], name='unique_follow')
+            models.UniqueConstraint(fields=['user', 'author'],
+                                    name='unique_follow'
+                                    )
         ]
         verbose_name = 'Подписка'
         verbose_name_plural = 'Подписки'
